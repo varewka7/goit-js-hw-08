5ecd6bc8f0b5d600632f9afda97b343501b115e2
Oksana Kozak <varewka7@gmail.com>
2023-01-20 15:29:09 +0200

    hw to check

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+
+function onPlay(e) {
+    localStorage.setItem('videoplayer-current-time', e.seconds); 
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+console.log(localStorage)
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+
+        default:
+            break;
+    }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,54 @@
+import throttle from 'lodash.throttle';
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+    input: document.querySelector('.feedback-form input'),
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
+populateTextarea();
+
+const formData = {};
+
+function onFormSubmit(evt) {
+    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+    evt.preventDefault();
+
+    evt.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
+}
+
+function onTextareaInput(evt) {
+    const messageOn = evt.target.value;
+
+    localStorage.setItem('feedback-form-state', messageOn);
+}
+
+function populateTextarea() {
+    const savedMessage = localStorage.getItem('feedback-form-state');
+
+    if (savedMessage) {
+        refs.textarea.value = savedMessage;
+    };
+}
+
+refs.form.addEventListener('input', e => {
+
+    formData[e.target.name] = e.target.value;
+    
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData))
+    const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+    if (data) {
+        refs.input = data.email;
+        refs.textarea = data.message;
+}
+})
+
+
+
+
 
